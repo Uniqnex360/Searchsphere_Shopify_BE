961a9b0a94ad06a19725e6009e2d49caec536dac
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-05-25 11:56:03 +0530

    added shopify sync function

--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -1,4 +1,4 @@
-from sqlmodel import select
+from sqlmodel import select, Session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.models import Store
@@ -26,6 +26,43 @@ async def get_store_id_by_shop(
     result = await db_session.exec(
         select(Store.id).where(Store.shop_domain == shop_domain)
     )
+
+    store_id = result.first()
+
+    if not store_id:
+        return None
+
+    # ==========================================
+    # SAVE CACHE
+    # ==========================================
+    STORE_CACHE[shop_domain] = int(store_id)
+
+    return int(store_id)
+
+
+# ==========================================
+# SYNC VERSION
+# ==========================================
+
+
+def get_store_id_by_shop_sync(
+    shop_domain: str,
+    db_session: Session,
+) -> int | None:
+    """
+    Sync version for normal SQLModel Session
+    """
+
+    # ==========================================
+    # CACHE HIT
+    # ==========================================
+    if shop_domain in STORE_CACHE:
+        return STORE_CACHE[shop_domain]
+
+    # ==========================================
+    # DB FETCH
+    # ==========================================
+    result = db_session.exec(select(Store.id).where(Store.shop_domain == shop_domain))
 
     store_id = result.first()
 
--- app/routes/sync_shopify.py
+++ app/routes/sync_shopify.py
@@ -2,6 +2,8 @@ from sqlmodel import Session
 from fastapi import APIRouter, Depends
 from sqlalchemy import select
 
+from app.auth import ShopifySession, verify_shopify_token
+from app.helpers import get_store_id_by_shop_sync
 from app.services import create_index, sync_shopify_to_elasticsearch
 from app.models import Store
 from app.database import get_sync_session
@@ -19,8 +21,14 @@ def create_elastic_search_index():
 @router.post("/sync-data/")
 def sync_shopify_to_post_es(
     session: Session = Depends(get_sync_session),
+    shopify_session: ShopifySession = Depends(verify_shopify_token),
 ):
-    store = session.execute(select(Store).where(Store.id == 4)).scalar_one_or_none()
+    store_id = get_store_id_by_shop_sync(
+        shop_domain=shopify_session.shop, db_session=session
+    )
+    store = session.execute(
+        select(Store).where(Store.id == store_id)
+    ).scalar_one_or_none()
 
     if not store:
         return {
